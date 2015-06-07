d765871d4d85eb61d76b7b23982b9c68f53d3c89
conradfeyt <conrad.feyt@gmail.com>
2015-06-07 18:25:39 +0200

    now more robust and less buggy
    Updated algorithm that calculates the amount of items spawned.
    Pauses the marquee on window blur and resumes on window focus (fixes overlapping issues)

--- marquee.js
+++ marquee.js
@@ -4,17 +4,17 @@
 
 * Author: Conrad Feyt
 * Email: conrad.feyt@gmail.com
-* Version: 1.1
+* Version: 1.5
 * Tested only on modern browsers
 
 
 // Structure //
 
-  ************************************* - container_class - ************************************
+  *********************************** - marque-container - *************************************
   *                                                                                            *
   *   ******************************* ******************************************************   *
   *   *                             * *                                                    *   *
-  *   *      - sibling_class -      * *                  - marquee_class -                 *   *
+  *   * - marquee-content-sibling - * *                 - marquee-content -                *   *
   *   *                             * *                                                    *   *
   *   ******************************* ******************************************************   *
   *                                                                                            *
@@ -22,26 +22,34 @@
 
 // Usage //
   
-Only need to call the createMarquee() function. Optionally: pass through the following paramaters to overwrite defaults:
-
+    Only need to call the createMarquee() function,
+    if desired, pass through the following paramaters:
 
     $1 duration:                   controls the speed at which the marquee moves
 
     $2 padding:                    right margin between consecutive marquees. 
 
-    $3 marquee_class:              class of the actual div or span that will be used to create the marquee - 
+    $3 marquee_class:             the actual div or span that will be used to create the marquee - 
                                    multiple marquee items may be created using this item's content. 
                                    This item will be removed from the dom
 
-    $4 container_class:            the container div in which the marquee content will animate. 
+    $4 container_class:           the container div in which the marquee content will animate. 
 
-    $5 hover:                      Boolean to indicate whether pause on hover should is required. 
-
-    $6 sibling_class:              a sibling item to the marqueed item  that 
+    $5 marquee-content-sibling :   (optional argument) a sibling item to the marqueed item  that 
                                    affects the end point position and available space inside the 
                                    container. 
 
-  Eg:  createMarquee({duration:30000, padding:20, marquee_class:'.example-marquee', container_class: '.example-container', sibling_class: '.example-sibling', hover: false});
+    $6 hover:                     Boolean to indicate whether pause on hover should is required. 
+  
+    Eg:
+        createMarquee({
+          duration: 20000,
+          padding: 10,
+          marquee_class: '.marquee',
+          container_class: '.marq-contaniner',
+          sibling_class: 'marq-sibling',
+          hover: true
+        });
 
 * ============================================================================================================== *
 
@@ -49,6 +57,16 @@ Only need to call the createMarquee() function. Optionally: pass through the fol
 
 
   var marqueeSpawned = [];
+  var marqueeHovered = false;
+
+
+  if(document.hasFocus()) {
+    var focused = true;
+  } else {
+    var focused = false;
+  }
+
+
   
   function marqueeObj (newElement) {
     this.el = newElement;
@@ -58,26 +76,29 @@ Only need to call the createMarquee() function. Optionally: pass through the fol
     this.timeLeft = 0;
     this.currentPos = 0;
     this.distanceLeft = 0;
-    this.totalLength = 0;
+    this.totalDistance = 0;
     this.contentWidth = 0;
     this.endPoint = 0;
     this.duration = 0;
     this.hovered = false;
+    this.padding = 0;
   }
 
 
   function getCurrentPosition() {
-    this.currentPos = parseInt($(this.name).css('margin-left'));
+    this.currentPos = parseInt($(this.name).css('left'));
     return this.currentPos;
   }
 
   function createMarquee(settings) {
 
-      var defaults = {
+
+  var defaults = {
         duration: 20000,
         padding: 10,
         marquee_class: '.marquee',
         container_class: '.container',
+
         sibling_class: 0,
         hover: true
       };
@@ -106,52 +127,71 @@ Only need to call the createMarquee() function. Optionally: pass through the fol
       }
 
 
+      var config = $.extend({}, defaults, settings);
+
       var marqueeContent =  $(config.marquee_class).html()
       var containerWidth = $(config.container_class).width();
       var contentWidth = $(config.marquee_class).width();
 
-      
       if (config.sibling_class == 0) { 
         var widthToIgnore = 0;
       } else {
         var widthToIgnore = $(config.sibling_class).width();
       }
 
-      var endPoint = -(contentWidth - widthToIgnore);
-      var totalLength =  containerWidth - endPoint;
-
       var spawnAmount = Math.ceil(containerWidth / contentWidth);
       //init vars from input
 
 
-      console.log(config);
-
       $(config.marquee_class).remove();
 
-      if(spawnAmount<2){
-          spawnAmount =2;
+      if(spawnAmount<=2){
+          spawnAmount =3;
+      } else {
+        spawnAmount++
       }
+
+      var totalContentWidth = (contentWidth + config.padding)*spawnAmount;
+
+      var endPoint = -(totalContentWidth - containerWidth);
+
+      var totalDistance =  containerWidth - endPoint;
+
+
       //initialise positions counters, content 
 
       for (i = 0; i < spawnAmount; i++) {
 
           if(config.hover == true){
 
+            
             var newElement = $('<div class="marquee-' + (i+1) + '">' + marqueeContent + '</div>')        
             .mouseenter(function() {
 
+
+              if ((focused == true) && (marqueeHovered == false)){
+                marqueeHovered = true;
+
                 for (var key in marqueeSpawned){
-                  marqueeSpawned[key].el.clearQueue().stop();
+                  marqueeSpawned[key].el.clearQueue().stop(); 
                   marqueeSpawned[key].hovered = true;
                 }
+                
+
+              }
 
             })
             .mouseleave(function() {
 
-                for (var key in marqueeSpawned){
-                  marqueeManager(marqueeSpawned[key]);   
-                } 
 
+                if ((focused == true) && (marqueeHovered == true)){
+
+                  for (var key in marqueeSpawned){
+                    marqueeManager(marqueeSpawned[key]);   
+                  } 
+
+                  marqueeHovered = false;
+                } 
             });
 
           } else {
@@ -167,19 +207,20 @@ Only need to call the createMarquee() function. Optionally: pass through the fol
           marqueeSpawned[i].currentPos = (widthToIgnore + (contentWidth*i))+(config.padding*i);  //initial positioning
           marqueeSpawned[i].name = '.marquee-'+(i+1); 
 
-          marqueeSpawned[i].totalLength = totalLength;  
+          marqueeSpawned[i].totalDistance = totalDistance;  
           marqueeSpawned[i].containerWidth = containerWidth;  
           marqueeSpawned[i].contentWidth = contentWidth;  
           marqueeSpawned[i].endPoint = endPoint;  
           marqueeSpawned[i].duration = config.duration;  
+          marqueeSpawned[i].padding = config.padding;  
 
-          $(marqueeSpawned[i].name).css('margin-left', marqueeSpawned[i].currentPos+config.padding +'px'); //setting margin according to postition
+          marqueeSpawned[i].el.css('left', marqueeSpawned[i].currentPos+config.padding +'px'); //setting left according to postition
 
-          
-          marqueeManager(marqueeSpawned[i]);
+           if (focused == true){
+            marqueeManager(marqueeSpawned[i]);
+          }
 
       }
-
   }
 
   function marqueeManager(marqueed_el) {
@@ -188,28 +229,46 @@ Only need to call the createMarquee() function. Optionally: pass through the fol
 
             if (marqueed_el.counter > 0) {  //this is not the first loop
               
-                marqueed_el.timeLeft = marqueed_el.duration;
-                marqueed_el.el.css('margin-left', marqueed_el.containerWidth +'px'); //setting margin according to postition
-                marqueed_el.currentPos = marqueed_el.containerWidth; //setting margin according to postition
+                  marqueed_el.timeLeft = marqueed_el.duration;
+                  marqueed_el.el.css('left', marqueed_el.containerWidth +'px'); //setting margin 
+                  marqueed_el.currentPos = marqueed_el.containerWidth; 
+                  marqueed_el.distanceLeft = marqueed_el.totalDistance - (marqueed_el.containerWidth - marqueed_el.getPosition());
 
             } else {    // this is the first loop
-              marqueed_el.timeLeft = (((marqueed_el.totalLength - (marqueed_el.containerWidth - marqueed_el.getPosition()))/ marqueed_el.totalLength)) * marqueed_el.duration;
+              
+              marqueed_el.timeLeft = (((marqueed_el.totalDistance - (marqueed_el.containerWidth - marqueed_el.getPosition()))/ marqueed_el.totalDistance)) * marqueed_el.duration;
             }
 
         } else {
               marqueed_el.hovered = false;
-              marqueed_el.currentPos = parseInt(marqueed_el.el.css('margin-left'));
-              marqueed_el.distanceLeft = marqueed_el.totalLength - (marqueed_el.containerWidth - marqueed_el.getPosition());
-              marqueed_el.timeLeft = (((marqueed_el.totalLength - (marqueed_el.containerWidth - marqueed_el.currentPos))/ marqueed_el.totalLength)) * marqueed_el.duration;
+              marqueed_el.currentPos = parseInt(marqueed_el.el.css('left'));
+              marqueed_el.distanceLeft = marqueed_el.totalDistance - (marqueed_el.containerWidth - marqueed_el.getPosition());
+              marqueed_el.timeLeft = (((marqueed_el.totalDistance - (marqueed_el.containerWidth - marqueed_el.currentPos))/ marqueed_el.totalDistance)) * marqueed_el.duration;
         }
 
     marqueeAnim(marqueed_el);
   }
 
   function marqueeAnim (marqueeObject){
-    marqueeObject.counter++;
-    marqueeObject.el.clearQueue().animate({'marginLeft': marqueeObject.endPoint+'px'}, marqueeObject.timeLeft, 'linear', function(){marqueeManager(marqueeObject)});
+      marqueeObject.counter++
+      marqueeObject.el.clearQueue().animate({'left': marqueeObject.endPoint+'px'}, marqueeObject.timeLeft, 'linear', function(){marqueeManager(marqueeObject)});
   }
 
+
+  window.onfocus = function(){
+    focused = true;
+      for (var key in marqueeSpawned){
+          marqueeManager(marqueeSpawned[key]);   
+      } 
+    };
+
+
+  window.onblur = function(){
+    focused = false;
+    for (var key in marqueeSpawned){
+        marqueeSpawned[key].el.clearQueue().stop(); 
+        marqueeSpawned[key].hovered = true;
+    }
+  };
 
 
